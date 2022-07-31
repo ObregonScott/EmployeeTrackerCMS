178f9f4294648d558562262297d5cbb8ab3ff16e
Scott Obregon <obregon.scott@gmail.com>
2022-07-31 03:00:07 -0500

    going back to the old way

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,33 +1,28 @@
-use employees;
-
-INSERT INTO department
-(name)
+INSERT INTO department 
 VALUES
-('Operations'), 
-('Finance'),
-('Legal'),
-('Engineering');
+(1, 'Engineering'), 
+(2, 'Finance'),
+(3, 'Legal'), 
+(4, 'Managment'),
+(5, 'Operations');
 
-INSERT INTO role
-(title, salary, department_id)
+INSERT INTO role (id, title, salary, department_id)
 VALUES
-('Operations Lead', 650000, 1),
-('Sales', 55000, 1), 
-('Lead Engineer', 150000, 2), 
-('Electrical Engineer', 122000, 2), 
-('Coordinator', 80000, 3),
-('Janitor', 999999, 3),
-('Lawyer', 150000, 4)
-('Paralegal', 60000,4)
+(1, 'manager', 100000, 5),
+(2, 'salesperson', 50000, 2), 
+(3, 'engineer', 60000, 1), 
+(4, 'researcher', 70000, 3), 
+(5, 'coordinator', 80000, 4);
 
-INSERT INTO employee
-(first_name, last_name, role_id, manager_id) 
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id) 
 VALUES
-('Barack', 'Obama', 1, 1),
-('Scott', 'Obregon', 1, 2),
-('Mike', 'Chan', 3, NULL),
-('Kevin', 'Tupik', 4, NULL),
-('Kunal', 'Singh', 5, NULL),
-('Sara', 'Lourd', 6, NULL),
-('Tom', 'Allen', 7, NULL),
-('Karen', 'Kerrayzee', 8, NULL)
+(1, 'Mike', 'Chan', 2, 10),
+(2, 'Ashley', 'Rodriguez', 2, 10),
+(3, 'Kevin', 'Tupik', 4, 10),
+(4, 'Kunal', 'Singh', 5, 10),
+(5, 'Malia', 'Brown', 2, 10),
+(6, 'Sara', 'Lourd', 3, 10),
+(7, 'Tom', 'Allen', 3, 10),
+(8, 'Scott', 'Obregon', 4, 10),
+(9, 'Karen', 'Kerrayzee', 5, 10),
+(10, 'Barack', 'Obama', 1, 10);
