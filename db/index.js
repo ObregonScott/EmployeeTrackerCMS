74d0a514f1f6819f88bc159a1e21a393f2f9e80f
Scott Obregon <obregon.scott@gmail.com>
2022-07-30 20:35:52 -0500

    working on DB within Index

--- db/index.js
+++ db/index.js
@@ -1,2 +1,25 @@
 const connection = require('./connection.js')
 
+class DB {
+    
+    constructor(connection) {
+        this.connection = connection;
+      }
+
+// Find Employees
+
+      findAllEmployees() {
+        return this.connection.promise().query(
+          "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
+        );
+      }
+
+// Find Managers
+
+      findAllPossibleManagers(employeeId) {
+        return this.connection.promise().query(
+          "SELECT id, first_name, last_name FROM employee WHERE id != ?",
+          employeeId
+        );
+      }
+}
